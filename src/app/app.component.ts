e6077662a18d54adb11a6c05132b6d409be5cb86
Gagan Khanna <gagan.khanna@avanade.com>
2023-09-08 12:24:05 -0600

    Landing page improvements. (Fix addresses #1)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  // initiate an empty string to hold the input value
+  newItem ='';
+  // initiate an empty array to hold the items
+  items:string[] = [];
+
+// write a function to add items to the list
+addItem() {
+  this.items.push(this.newItem);
+  this.newItem = '';
+}
+
+// write a function to remove items to the list
+removeItem(item: string) {
+  this.items = this.items.filter((i: any) => {
+    return i !== item;
+  })
+}
+
   title = 'chips-input';
 }
